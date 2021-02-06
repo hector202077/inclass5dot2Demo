0690880f4088997b52270693e7f75d8e6b5f54ee
student <hectormrg0823@gmail.com>
2021-02-06 15:20:44 +0000

    add logic and test

---
+++ src/main/java/com/demo5dot2/demo5dot2/Demo5dot2Application.java
@@ -0,0 +1,13 @@
+package com.demo5dot2.demo5dot2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo5dot2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo5dot2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo5dot2/demo5dot2/Demo5dot2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo5dot2.demo5dot2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo5dot2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
